e09d447f1ada5839bd4ae8656cd304ee55334748
lycHub <1120805364@qq.com>
2018-07-08 19:17:38 +0800

    4-6

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: ['', [Validators.email, this.validate]],
+      password: [null, Validators.required]
+    });
+  }
 
   ngOnInit() {
   }
 
+  onSubmit({value, valid}, evt: Event) {
+    evt.preventDefault();
+    console.log(value, valid);
+
+    // 临时指定验证器
+    // this.form.controls['email'].setValidators(this.validate);
+  }
+
+
+  private validate(c: FormControl): {[key: string]: any} {
+    if (!c.value) {
+      return null;
+    } else {
+      const pattern = /^wang+/;
+      if (pattern.test(c.value)) {
+        return null;
+      } else {
+        return {emailNotValid: '要以wang开头'};
+      }
+    }
+  }
 }
